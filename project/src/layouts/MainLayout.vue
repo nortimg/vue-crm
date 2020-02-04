7370151f356e1a46d2d85b84f826a424df54ca4f
Alexey Gmitron <gmitron01@gmail.com>
2020-02-04 07:37:43 +0300

    Add the main layouts

---
+++ project/src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="app-main-layout">
+      <Sidebar v-model="isOpen" />
+      <Navbar @click="isOpen = !isOpen" />
+
+      <main class="app-content" :class="{full: !isOpen}">
+        <div class="app-page">
+          <router-view />
+        </div>
+      </main>
+
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large blue" href="#">
+          <i class="large material-icons">add</i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/app/Navbar'
+import Sidebar from '@/components/app/Sidebar'
+
+
+export default {
+  name: 'main-layout', 
+  components: {
+    Navbar, 
+    Sidebar
+  }, 
+  data: () => ({
+    isOpen: true, 
+  })
+}
+</script>
---
+++ project/src/router/index.js
@@ -0,0 +1,87 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/', 
+    name: 'home', 
+    component: () => import('../views/Home.vue'), 
+    meta: {
+      layout: 'main'
+    }
+  }, 
+  {
+    path: '/login', 
+    name: 'login', 
+    component: () => import('../views/Login.vue'), 
+    meta: {
+      layout: 'empty'
+    }
+  }, 
+  {
+    path: '/register', 
+    name: 'register', 
+    component: () => import('../views/Register.vue'), 
+    meta: {
+      layout: 'empty'
+    }
+  },
+  {
+    path: '/categories', 
+    name: 'categories', 
+    component: () => import('../views/Categories.vue'), 
+    meta: {
+      layout: 'main'
+    }
+  }, 
+  {
+    path: '/detail-record', 
+    name: 'detail-record', 
+    component: () => import('../views/DetailRecord.vue'), 
+    meta: {
+      layout: 'main'
+    }
+  }, 
+  {
+    path: '/planning', 
+    name: 'planning', 
+    component: () => import('../views/Planning.vue'), 
+    meta: {
+      layout: 'main'
+    }
+  }, 
+  {
+    path: '/profile', 
+    name: 'profile', 
+    component: () => import('../views/Profile.vue'), 
+    meta: {
+      layout: 'main'
+    }
+  }, 
+  {
+    path: '/record', 
+    name: 'record', 
+    component: () => import('../views/Record.vue'), 
+    meta: {
+      layout: 'main'
+    }
+  }, 
+  {
+    path: '/history', 
+    name: 'history', 
+    component: () => import('../views/History.vue'), 
+    meta: {
+      layout: 'main'
+    }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
